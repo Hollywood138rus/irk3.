785df1fbbf12ab44f1421db6474e42528bb7f4e7
Багиров Камиль <bagirovkamil@MacBook-Pro-Bagirov.local>
2024-02-05 17:48:58 +0300

    refactor: изменил функцию setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
 
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ipython>=6", "nbformat>=4", "nbconvert>=5", "requests>=2"]
+
+setup(
+    name="irk3.1",
+    version="0.0.1",
+    author="Hollywood138",
+    author_email="bagirovkamil757@gmail.com",
+    description="A package to convert your Jupyter Notebook",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hollywood138rus/irk3.1.git",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
